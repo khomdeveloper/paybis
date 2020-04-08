230ad4e83c6598ecafecc1b18c353d33901b07c9
Валерий Хомяков <vh@1september.ru>
2020-04-08 16:58:45 +0300

    maker

--- src/Controller/ActionController.php
+++ src/Controller/ActionController.php
@@ -23,13 +23,15 @@ class ActionController extends AbstractController{
     {
         try {
         
-        $login = filter_var(substr(trim($request->request->get('login')),0,10), FILTER_VALIDATE_EMAIL);
-        $pass = substr(trim($request->request->get('pass')),0,10);
+        $login = filter_var(substr(trim($request->request->get('login')),0,50), FILTER_VALIDATE_EMAIL);
+        $pass = substr(trim($request->request->get('pass')),0,50);
         if (empty($login) || empty($pass)){
             throw new \Exception('Login or pass not valid');
         }
         
         var_dump($login);
+        
+        
         
         } catch (\Exception $e) {
           die($e->getMessage());   
