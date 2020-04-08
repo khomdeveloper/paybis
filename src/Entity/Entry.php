89affe8407071ff1132b23ccd2f97adfd76b6c54
Валерий Хомяков <vh@1september.ru>
2020-04-08 13:49:30 +0300

    mysql

---
+++ src/Entity/Entry.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM; 
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Entry
+ *
+ * @author valera261104
+ */
+class Entry {
+    //put your code here
+}
