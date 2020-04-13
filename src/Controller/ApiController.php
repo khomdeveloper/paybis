a03aa2834c104fc87382d7323a5ea2417f29d86a
Валерий Хомяков <vh@1september.ru>
2020-04-13 13:11:38 +0300

    api controller

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -24,7 +24,7 @@ class ApiController {
      
         $dataBaseService = new DatabaseStorageService($this->doctrine);
         
-        $dataBaseServiсe->getList();
+        $dataBaseService->getList();
         
     }
     
