e8c350d14335c6389ce3e424651fe82d19736a31
Валерий Хомяков <vh@1september.ru>
2020-04-13 21:37:20 +0300

    ORM data source

--- src/Entity/RateSource.php
+++ src/Entity/RateSource.php
@@ -13,7 +13,7 @@ use Doctrine\ORM\Mapping as ORM;
 class RateSource
 {
     /**
-     * @ORM\Column(type="integer)
+     * @ORM\Column(type="integer")
      * @ORM\Id
      * @ORM\GeneratedValue(startegy="AUTO")
      */
