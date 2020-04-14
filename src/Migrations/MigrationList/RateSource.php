fb0879d1ae3c6960758a6d6bbbb44caa4cc8db4b
Валерий Хомяков <vh@1september.ru>
2020-04-14 14:30:31 +0300

    parse API response

--- src/Migrations/MigrationList/RateSource.php
+++ src/Migrations/MigrationList/RateSource.php
@@ -13,7 +13,7 @@ class RateSource implements MigrationSQLDeterminatorInterface
                     CREATE TABLE IF NOT EXISTS `rate_source` (
                       `id` int NOT NULL COMMENT 'Service id' AUTO_INCREMENT PRIMARY KEY,
                       `url` varchar(255) NOT NULL COMMENT 'Service url',
-                      `name` varchar (255) NOT NULL COMMENT 'Service name'
+                      `name` varchar (255) NOT NULL COMMENT 'Service name',
                       `method` enum ('POST','GET') NULL DEFAULT 'GET',
                       `parameters` varchar(255) NULL COMMENT 'Parameters to request (if necessary)',
                       `frequency` float NULL DEFAULT 1.0 COMMENT 'Frequency of request in seconds',
